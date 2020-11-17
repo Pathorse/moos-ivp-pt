8a7c0b6923c5701ac3162e5c67bd6adaab99cfee
Paal Arthur S. Thorseth <paalthorseth@gmail.com>
2020-11-17 19:52:14 +0100

    Save wip

--- missions/sbmpc_scenario_3/launch.sh
+++ missions/sbmpc_scenario_3/launch.sh
@@ -40,21 +40,45 @@ done
 VNAME1="os"           # The first vehicle Community
 VNAME2="cn_1"           # The second vehicle Community
 VNAME3="cn_2"           # The second vehicle Community
-START_POS1="0,-150"
-START_POS2="0,0"        
-START_POS3="150,-150"        
+VNAME4="cn_3"           # The second vehicle Community
+VNAME5="cn_4"           # The second vehicle Community
+VNAME6="cn_5"           # The second vehicle Community
+VNAME7="cn_6"           # The second vehicle Community
+START_POS1="0,-145"        # -50, -190 
+START_POS2="40,-105"
+START_POS3="-3,-27"
+START_POS4="40,-15"
+START_POS5="70,-155"
+START_POS6="120,-145"
+START_POS7="150,-145"
 START_HEADING1="45"
-START_HEADING2="135"
-START_HEADING3="315"
+START_HEADING2="45"
+START_HEADING3="135"
+START_HEADING4="135"
+START_HEADING5="335"
+START_HEADING6="335"
+START_HEADING7="335"
 SPD1="1.2"
-SPD2="1.2"
-SPD3="1.2"
-PTS1="{150,0}"
-PTS2="{150,-150}"
-PTS3="{0,0}"
+SPD2="0.5"
+SPD3="1.0"
+SPD4="1.0"
+SPD5="1.0"
+SPD6="1.0"
+SPD7="1.0"
+PTS1="{175,30}"
+PTS2="{175,30}"
+PTS3="{30,-155}"
+PTS4="{120,-120}"
+PTS5="{20,-25}"
+PTS6="{65,20}"
+PTS7="{65,20}"
 COLREGS_COMPLIANCE1="true"
 COLREGS_COMPLIANCE2="false"
 COLREGS_COMPLIANCE3="false"
+COLREGS_COMPLIANCE4="false"
+COLREGS_COMPLIANCE5="false"
+COLREGS_COMPLIANCE6="false"
+COLREGS_COMPLIANCE7="false"
 SHORE_LISTEN="9300"
 
 nsplug meta_vehicle.moos targ_$VNAME1.moos -f WARP=$TIME_WARP \
@@ -71,6 +95,26 @@ nsplug meta_vehicle.moos targ_$VNAME3.moos -f WARP=$TIME_WARP \
     VNAME=$VNAME3          SHARE_LISTEN="9303"            \
     VPORT="9003"           SHORE_LISTEN=$SHORE_LISTEN     \
     START_POS=$START_POS3  START_HEADING=$START_HEADING3
+
+nsplug meta_vehicle.moos targ_$VNAME4.moos -f WARP=$TIME_WARP \
+    VNAME=$VNAME4          SHARE_LISTEN="9304"            \
+    VPORT="9004"           SHORE_LISTEN=$SHORE_LISTEN     \
+    START_POS=$START_POS4  START_HEADING=$START_HEADING4
+
+nsplug meta_vehicle.moos targ_$VNAME5.moos -f WARP=$TIME_WARP \
+    VNAME=$VNAME5          SHARE_LISTEN="9308"            \
+    VPORT="9008"           SHORE_LISTEN=$SHORE_LISTEN     \
+    START_POS=$START_POS5  START_HEADING=$START_HEADING5
+
+nsplug meta_vehicle.moos targ_$VNAME6.moos -f WARP=$TIME_WARP \
+    VNAME=$VNAME6          SHARE_LISTEN="9306"            \
+    VPORT="9006"           SHORE_LISTEN=$SHORE_LISTEN     \
+    START_POS=$START_POS6  START_HEADING=$START_HEADING6
+
+nsplug meta_vehicle.moos targ_$VNAME7.moos -f WARP=$TIME_WARP \
+    VNAME=$VNAME7          SHARE_LISTEN="9307"            \
+    VPORT="9007"           SHORE_LISTEN=$SHORE_LISTEN     \
+    START_POS=$START_POS7  START_HEADING=$START_HEADING7
 
 nsplug meta_shoreside.moos targ_shoreside.moos -f WARP=$TIME_WARP \
     SNAME="shoreside"  SHARE_LISTEN=$SHORE_LISTEN                 \
@@ -90,12 +134,36 @@ nsplug meta_vehicle.bhv targ_$VNAME3.bhv -f VNAME=$VNAME3               \
     START_POS=$START_POS3                       PTS=$PTS3          \
     COLREGS_COMPLIANCE=$COLREGS_COMPLIANCE3     TRANSIT_SPD=$SPD3
 
+nsplug meta_vehicle.bhv targ_$VNAME4.bhv -f VNAME=$VNAME4               \
+    START_POS=$START_POS4                       PTS=$PTS4          \
+    COLREGS_COMPLIANCE=$COLREGS_COMPLIANCE4     TRANSIT_SPD=$SPD4
+
+nsplug meta_vehicle.bhv targ_$VNAME5.bhv -f VNAME=$VNAME5               \
+    START_POS=$START_POS5                       PTS=$PTS5          \
+    COLREGS_COMPLIANCE=$COLREGS_COMPLIANCE5     TRANSIT_SPD=$SPD6
+
+nsplug meta_vehicle.bhv targ_$VNAME6.bhv -f VNAME=$VNAME6               \
+    START_POS=$START_POS6                       PTS=$PTS6          \
+    COLREGS_COMPLIANCE=$COLREGS_COMPLIANCE6     TRANSIT_SPD=$SPD6
+
+nsplug meta_vehicle.bhv targ_$VNAME7.bhv -f VNAME=$VNAME7               \
+    START_POS=$START_POS7                       PTS=$PTS7          \
+    COLREGS_COMPLIANCE=$COLREGS_COMPLIANCE7     TRANSIT_SPD=$SPD7
+
 if [ ! -e targ_$VNAME1.moos ]; then echo "no targ_$VNAME1.moos"; exit; fi
 if [ ! -e targ_$VNAME1.bhv  ]; then echo "no targ_$VNAME1.bhv "; exit; fi
 if [ ! -e targ_$VNAME2.moos ]; then echo "no targ_$VNAME2.moos"; exit; fi
 if [ ! -e targ_$VNAME2.bhv  ]; then echo "no targ_$VNAME2.bhv "; exit; fi
 if [ ! -e targ_$VNAME3.moos ]; then echo "no targ_$VNAME3.moos"; exit; fi
 if [ ! -e targ_$VNAME3.bhv  ]; then echo "no targ_$VNAME3.bhv "; exit; fi
+if [ ! -e targ_$VNAME4.moos ]; then echo "no targ_$VNAME4.moos"; exit; fi
+if [ ! -e targ_$VNAME4.bhv  ]; then echo "no targ_$VNAME4.bhv "; exit; fi
+if [ ! -e targ_$VNAME5.moos ]; then echo "no targ_$VNAME5.moos"; exit; fi
+if [ ! -e targ_$VNAME5.bhv  ]; then echo "no targ_$VNAME5.bhv "; exit; fi
+if [ ! -e targ_$VNAME6.moos ]; then echo "no targ_$VNAME6.moos"; exit; fi
+if [ ! -e targ_$VNAME6.bhv  ]; then echo "no targ_$VNAME6.bhv "; exit; fi
+if [ ! -e targ_$VNAME7.moos ]; then echo "no targ_$VNAME7.moos"; exit; fi
+if [ ! -e targ_$VNAME7.bhv  ]; then echo "no targ_$VNAME7.bhv "; exit; fi
 if [ ! -e targ_shoreside.moos ]; then echo "no targ_shoreside.moos";  exit; fi
 
 if [ ${JUST_MAKE} = "yes" ] ; then
@@ -114,6 +182,14 @@ echo "Launching $VNAME2 MOOS Community with WARP:" $TIME_WARP
 pAntler targ_$VNAME2.moos >& /dev/null &
 echo "Launching $VNAME3 MOOS Community with WARP:" $TIME_WARP
 pAntler targ_$VNAME3.moos >& /dev/null &
+echo "Launching $VNAME4 MOOS Community with WARP:" $TIME_WARP
+pAntler targ_$VNAME4.moos >& /dev/null &
+echo "Launching $VNAME5 MOOS Community with WARP:" $TIME_WARP
+pAntler targ_$VNAME5.moos >& /dev/null &
+echo "Launching $VNAME6 MOOS Community with WARP:" $TIME_WARP
+pAntler targ_$VNAME6.moos >& /dev/null &
+echo "Launching $VNAME7 MOOS Community with WARP:" $TIME_WARP
+pAntler targ_$VNAME7.moos >& /dev/null &
 echo "Done "
 
 uMAC targ_shoreside.moos
